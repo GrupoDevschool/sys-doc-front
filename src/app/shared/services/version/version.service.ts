b3b2c2c9eea00f80b5ce400c82ba573a4ba69c2a
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2021-10-15 11:45:24 -0300

    Some version service

--- src/app/shared/services/version/version.service.ts
+++ src/app/shared/services/version/version.service.ts
@@ -1,9 +1,25 @@
+import { CreateVersion, Version } from './../../model/Version';
 import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/core/api/api.service';
+import { Observable } from 'rxjs';
 
+const URL = '/version';
 @Injectable({
   providedIn: 'root'
 })
 export class VersionService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  create(version: CreateVersion): Observable<CreateVersion> {
+    return this.api.post(URL, version);
+  }
+
+  update(version: Version): Observable<Version> {
+    return this.api.put(URL + '/' + version.id, version);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.api.delete(URL + '/' + id);
+  }
 }
