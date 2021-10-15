be2ddd24e247ebeb64d398e107a2e6d77b529e19
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2021-10-15 12:06:57 -0300

    screen service

--- src/app/shared/services/screen/screen.service.ts
+++ src/app/shared/services/screen/screen.service.ts
@@ -1,9 +1,35 @@
+import { Version } from './../../model/Version';
+import { ApiService } from './../../../core/api/api.service';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { createScreen } from '../../model/Screen';
 
+const URL = "/screen";
 @Injectable({
   providedIn: 'root'
 })
 export class ScreenService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  getAll(versionId: number): Observable<Screen[]> {
+    return this.api.getAll(URL + '/' + versionId);
+  }
+
+  getById(id: number): Observable<Screen> {
+    return this.api.get(URL + '/' + id);
+  }
+
+  create(version: createScreen): Observable<createScreen> {
+    return this.api.post(URL, version);
+  }
+
+  update(version: createScreen): Observable<createScreen> {
+    return this.api.put(URL + '/' + version.id, version);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.api.delete(URL + '/' + id);
+  }
+
 }
