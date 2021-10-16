f3277baba71176a9c0a748ab116d0f82826d040a
Thales M P Macena <thales.macena@akross.com.br>
2021-10-16 14:31:06 -0300

    modulos

--- src/app/shared/module/material.module.ts
+++ src/app/shared/module/material.module.ts
@@ -3,17 +3,23 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
 
 export const MODULES = [
   MatButtonModule,
@@ -29,7 +35,14 @@ export const MODULES = [
   MatProgressSpinnerModule,
   MatTooltipModule,
   MatListModule,
-  MatMenuModule
+  MatMenuModule,
+  MatDatepickerModule,
+  MatDividerModule,
+  MatPaginatorModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatTabsModule
 ]
 
 
