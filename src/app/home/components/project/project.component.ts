87931b1bd49970e63b7f97564ccd81e0a3265235
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2021-10-18 16:23:54 -0300

    tela de listagem projetos

--- src/app/home/components/project/project.component.ts
+++ src/app/home/components/project/project.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ProjectService } from './../../../shared/services/project/project.service';
+import { Project } from './../../../shared/model/Project';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements AfterViewInit {
 
-  constructor() { }
+  project?: Project[];
+  displayedColumns: string[] = ['id', 'name', 'status'];
+  dataSource: MatTableDataSource<Project>;
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private projectsService: ProjectService ) { }
+
+  ngAfterViewInit() {
+    this.projectsService.getAll().subscribe((projects) => {
+      this.dataSource = new MatTableDataSource(projects)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+    )
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
 }
+
+
+
