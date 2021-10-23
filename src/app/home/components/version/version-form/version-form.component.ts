adc9f9f26ccb6c4566f74cdc8020135e8514bf63
jorgeaugustocarvalho <javgccarvalho@gmail.com>
2021-10-23 17:01:51 -0300

    Version-form component.ts

--- src/app/home/components/version/version-form/version-form.component.ts
+++ src/app/home/components/version/version-form/version-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CreateVersion, Version } from 'src/app/shared/model/Version';
+import { VersionService } from 'src/app/shared/services/version/version.service';
 
 @Component({
   selector: 'app-version-form',
@@ -7,9 +13,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VersionFormComponent implements OnInit {
 
-  constructor() { }
+  versionForm!: FormGroup;
+  matcher = new ErrorStateMatcher;
+  loading: boolean = false;
+  updateVersion: Version | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private versionService: VersionService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    const state = this.router.getCurrentNavigation()?.extras?.state as Version;
+    this.updateVersion = state;
   }
 
+  ngOnInit(): void {
+    this.versionForm = this.formBuilder.group({
+      id: new FormControl(this.updateVersion?.id ?? null),
+      active: new FormControl(this.updateVersion?.active? true : false, [Validators.required]),
+      number: new FormControl(this.updateVersion?.number ?? '', [Validators.required]),
+
+    })
+  }
+
+  submit() {
+    if (this.versionForm.valid) {
+      const formFields = this.versionForm.getRawValue() as CreateVersion;
+
+      this.loading = true;
+
+      if (formFields.id) {
+        const updateVersion: CreateVersion = {
+          id: formFields.id,
+          versionNumber: formFields.versionNumber,
+          description: formFields.description,
+          deployDate: formFields.deployDate,
+          status: formFields.status,
+          order: formFields.order,
+          versionCloneId: formFields.versionCloneId,
+          projectId: formFields.projectId
+        }
+
+        this.versionService.update(updateVersion).subscribe(
+          data => {
+            this.showSucess("Versão atualizada com sucesso!");
+            this.router.navigate(['dashboard/project']);
+          },
+          error => {
+            this.showError(error.message);
+          }
+        ).add(() => {
+          this.loading = false;
+        });
+
+      } else {
+        const newVersion: CreateVersion = {
+          id: formFields.id,
+          versionNumber: formFields.versionNumber,
+          description: formFields.description,
+          deployDate: formFields.deployDate,
+          status: formFields.status,
+          order: formFields.order,
+          versionCloneId: formFields.versionCloneId,
+          projectId: formFields.projectId
+        }
+
+        this.versionService.create(newVersion).subscribe(
+          data => {
+            this.showSucess("Versão criada com sucesso!");
+            this.router.navigate(['dashboard/version']);
+          },
+          error => {
+            this.showError(error.message);
+          }
+        ).add(() => {
+          this.loading = false;
+        });
+      }
+    }
+  }
+
+  showError(message: string) {
+    this.toastr.error(message, "Formulário inválido")
+  }
+
+  showSucess(message: string) {
+    this.toastr.success(message, "Versão Salva")
+  }
 }
