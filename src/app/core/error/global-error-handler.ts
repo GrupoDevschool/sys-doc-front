c17514a1551b5703dfa8f38b1939bc62151cbb73
Thales M P Macena <thales.macena@akross.com.br>
2021-11-08 21:11:48 -0300

    Alteração no global error handler

--- src/app/core/error/global-error-handler.ts
+++ src/app/core/error/global-error-handler.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 import { LoginService } from 'src/app/login/login.service';
 
 @Injectable()
@@ -13,17 +14,22 @@ export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: any) {
     this.ngZone.run(() => {
-      if(error.status == 401 && error instanceof HttpErrorResponse) {
+      if((error.status === 401 || error.status === 403) && error instanceof HttpErrorResponse) {
+        this.toastrService.error("Seu token está expirado, faça login novamente", "Erro de autenticação");
         this.loginService.logout();
       }
-    });
 
-    if(error instanceof HttpErrorResponse == false)
-      console.log('error:', error);
+      if(error instanceof HttpErrorResponse == false) {
+        console.log('error:', error);
+      }
+    });
   }
 
   get router(): Router {
     return this.injector.get(Router);
   };
 
+  private get toastrService(): ToastrService {
+    return this.injector.get(ToastrService);
+  }
 }
